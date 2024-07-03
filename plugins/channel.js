045117bb93051f370f6f11ba30bbf0a02779f84d
Manoj xd <157263075+manojnath124@users.noreply.github.com>
2024-07-03 21:17:42 +0530

    Add files via upload

---
+++ plugins/channel.js
@@ -0,0 +1,16 @@
+let handler = async m => m.reply(`
+
+╭⭑⭑⭑★✪•••* 𝚳𝚫𝚴𝚯𝐉-𝚾𝐃*•••✪★⭑⭑⭑
+│ 📂 *BOT Name:* _ 𝚳𝚫𝚴𝚯𝐉-𝚾𝐃_
+│ 📝 *Description:* _I'm  𝚳𝚫𝚴𝚯𝐉-𝚾𝐃. A  
+┃MultiDevice WhatsApp bot with rich features  ┃Created BY +2347045035241._
+│ 👤 *Owner:* _ 𝚳𝚫𝚴𝚯𝐉_
+│ 🌐 *Channel:* https://whatsapp.com/channel
+┃/0029Va9wmuz8F2pGIURwmo0m
+╰━━━━━━━━━━━━━━━━╯
+`.trim())
+handler.help = ['channel']
+handler.tags = ['main']
+handler.command = ['star', 'channel' ] 
+
+export default handler
